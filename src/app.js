b51bd12a18257f8c7b68a0b40c7b555147c0b9ea
farihulrouf <farihulrouf@gmail.com>
2022-04-08 20:44:42 +0700

    nav

--- src/app.js
+++ src/app.js
@@ -3,6 +3,22 @@ import Logo from './images/logo.png'
 import Banner from './images/banner.png'
 import Ship from './images/ship.png'
 import product1 from './images/product1.png'
-console.log('!hola')
-console.log('hey oke ya!!')
-document.body.append(component())
+import menu from './images/menu.png'
+import close from './images/close.png'
+
+const bar = document.getElementById('bar')
+const clos = document.getElementById('close')
+const nav = document.getElementById('navbar')
+if(bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if(clos) {
+    clos.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+
